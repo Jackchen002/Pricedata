d74045af655a346e78696d8fd4b4f258bf595adf
Jackchen002 <160606161+Jackchen002@users.noreply.github.com>
2024-10-09 11:23:27 +0800

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import  requests
-import json
-import xlwt
+import requests
 
 url = "http://www.xinfadi.com.cn/getPriceData.html"
 
 param = {
-    "limit": 40,
+    "limit": 100,
     "current": 1,
     "pubDateStartTime": "2024/10/08",
     "pubDateEndTime": "2024/10/08",
@@ -16,17 +14,7 @@ param = {
 
 def download_one_page():
     resp = requests.post(url = url, params = param)
-
-    title = list(set([j for i in resp.json() for j in i]))
-    book = xlwt.Workbook()
-    sheet = book.add_sheet('Sheet1', cell_overwrite_ok=True)  # 添加一个sheet页
-    for i in range(len(title)):  # 循环列
-        sheet.write(0, i, title[i])  # 将title数组中的字段写入到0行i列中
-    for i, it in enumerate(resp.json()):
-        for j, k in enumerate(title):
-            sheet.write(1 + i, j, it[k])
-    book.save('Pricedata.xls')
-
+    print(resp.json())
     resp.close()
 
 
